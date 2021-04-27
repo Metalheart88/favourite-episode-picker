d26f7e2ecae74e7b793b2d2af81be744e50dd97b
Greg Domanin <gregorydomanin@gmail.com>
2021-04-27 00:48:58 -0500

    Added fetching of the episodes. Styled the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,63 @@
 import React from 'react'
 import { Store } from './Store'
 
+interface IEpisode {
+  airdate: string
+  airstamp: string
+  airtime: string
+  id: number
+  image: {
+    medium: string
+    original: string
+  }
+  name: string
+  number: number
+  runtime: number
+  season: number
+  summary: string
+  type: string
+  url: string
+}
+
 const App = (): JSX.Element => {
-  const store = React.useContext(Store)
+  const { state, dispatch } = React.useContext(Store)
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+  const fetchDataAction = async () => {
+    const URL =
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data = await fetch(URL)
+    const dataJSON = await data.json()
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes,
+    })
+  }
+
   return (
     <>
-      <h1>Rick and Morty</h1>
-      <p>Pick your favourite episode!</p>
-      {store.episodes}
+      <header className='header'>
+        <h1>Rick and Morty</h1>
+        <p>Pick your favourite episode!</p>
+      </header>
+      <section className='episode-layout'>
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id} className='episode-box'>
+              <img
+                src={episode.image?.medium}
+                alt={`Rick and Morty ${episode.name}`}
+              />
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season} Episode: {episode.number}
+              </section>
+            </section>
+          )
+        })}
+      </section>
     </>
   )
 }
--- src/Store.tsx
+++ src/Store.tsx
@@ -4,15 +4,33 @@ interface IState {
   episodes: string[]
   favourites: string[]
 }
+
+interface IAction {
+  type: string
+  payload: any
+}
+
 const initialState: IState = {
   episodes: [],
   favourites: [],
 }
 
-export const Store = React.createContext<IState>(initialState)
+export const Store = React.createContext<IState | any>(initialState)
 
-const reducer = () => {}
+const reducer = (state: IState, action: IAction): IState => {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      return { ...state, episodes: action.payload }
+    default:
+      return state
+  }
+}
 
 export const StoreProvider = (props: any): JSX.Element => {
-  return <Store.Provider value={initialState}>{props.children}</Store.Provider>
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Store.Provider>
+  )
 }
